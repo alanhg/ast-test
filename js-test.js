9c049db15b5fac853ec49c14df9efebe9471ead4
alanhe <qianghe421@gmail.com>
2021-11-27 19:51:29 +0800

    test

--- js-test.js
+++ js-test.js
@@ -1,4 +1,25 @@
 const esprima = require('esprima');
-const program = 'const answer = 42';
+let program = 'location.href="https://1991421.cn"';
 
-console.log(esprima.tokenize(program));
+let data = esprima.tokenize(program);
+data.forEach((item) => {
+  if ((typeof item.type) !== 'string') {
+    return;
+  }
+  const mark = item.value.charAt(0);
+  let res = item.value;
+  res = res.replace(/1991421\.cn/, `${mark}+${'window.basicHost'}+${mark}`)
+  if (res !== item.value) {
+    program = program.replace(item.value, res);
+  }
+})
+console.log(program);
+
+/**
+ * [
+ { type: 'Keyword', value: 'const' },
+ { type: 'Identifier', value: 'answer' },
+ { type: 'Punctuator', value: '=' },
+ { type: 'Numeric', value: '42' }
+ ]
+ */
